d4aa96dec24aa773ff6a2a414bde22f808abb8d0
Steve <falecomstevevaz@gmail.com>
2024-06-30 17:05:19 -0300

    Commit class_12 arithmatic operations

---
+++ 2_condicional_structure/class_12.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+    
+        // = 
+        // +, -, *, /, %
+
+        $x = 10;
+        $y = 8; 
+
+        $x -= $y;
+
+        echo "Value of x  = " . $x;
+
+    ?>
+
+</body>
+</html>
