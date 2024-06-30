68bb2f157b142380fae2f9c5b29a50a2c4a6b3be
Steve <falecomstevevaz@gmail.com>
2024-06-30 16:58:10 -0300

    Commit class_11 about the operators

---
+++ 2_condicional_structure/class_11.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    
+        $num1 = 12;
+        $num2 = 4;
+
+        echo "The sum between $num1 and $num2 is equal to " . ($num1 + $num2);
+        echo "<br />";
+        echo "The difference between $num1 and $num2 is equal to " . ($num1 - $num2);
+        echo "The multiplication between $num1 and $num2 is equal to " . ($num1 * $num2);
+        echo "<br />";
+        echo "The division between $num1 and $num2 is equal to " . ($num1 / $num2);
+        echo "<br />";
+        echo "The remainder between $num1 and $num2 is equal to " . ($num1 % $num2);
+    ?>
+
+</body>
+</html>
