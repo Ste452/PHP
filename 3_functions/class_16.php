1a174cae66657f4e81b5a28c6ed9df6ea316c87b
Steve <falecomstevevaz@gmail.com>
2024-07-31 12:17:05 -0300

    Lesson 16 Mathematical functions

---
+++ 3_functions/class_16.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+      <?php   
+      
+        $num = 7.3; // 2.0
+
+        echo ceil($num); // 
+        echo "<hr />";
+        echo floor($num);
+        echo "<hr />";
+        echo round($num);
+        echo "<hr />";
+        echo rand(10, 20);
+        echo "<hr />";
+        echo sqrt(73);
+
+      ?>
+
+</body>
+</html>
