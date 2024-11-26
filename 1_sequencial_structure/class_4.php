490a2abc264d15a8a092cbe99b88b226890e08e3
Steve <falecomstevevaz@gmail.com>
2024-11-25 23:18:42 -0300

    Adding more variables in class_4 for practicing

--- 1_sequencial_structure/class_4.php
+++ 1_sequencial_structure/class_4.php
@@ -8,11 +8,66 @@
 <body>
 
     <?php 
-    
-        $variable = "Variable";
 
+        /* String */
+        
+        /* Variable are sequences of characters that are delimeted of 
+        simple quotation or double quotes */
 
-        echo "this" . $variable . "is the type of String";
+        $String = "Variable";
+        var_dump($String);
+
+        /* Integer */
+        
+        // Numbers, positive or negative */
+        $integer = 42;
+        var_dump($integer);
+
+        /* Boolean */
+
+        // Are represent in a true of false value */
+        $bollean = true;
+        var_dump($bollean);
+
+        /* Compound types */
+
+        // These types are formed by multiple values ​​or structures.
+        $array = [1,2,3, "Text"];
+        var_dump($array);
+
+        /* Object */
+
+        // Class Instances defined by user
+        class Person {
+
+            public $name;
+            public function __construct($name) {
+                $this -> name = name;
+            }
+        }
+
+        $object = new Person("Steve");  
+        var_dump($object);
+
+        /* Especial Types */
+
+        // These represent references of special states in PHP
+        $null = null;
+        var_dump($null);
+
+        /* Resource */
+
+        // References to external resources, such as database connections or open files.
+        $file = fopen("example.txt", "r");
+        var_dump($file);
+
+        /* New and other types (PHP 7+) */
+        function hello() {
+            echo "Hello";
+        }
+
+        $calleble = "hello";
+        $calleble();
     
     ?>
 
