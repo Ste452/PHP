2bc43dd2d6ce36e4c73f4f5e259b0879af1d2f2d
Steve <falecomstevevaz@gmail.com>
2025-01-01 21:56:14 -0300

    Practicing Operators in PHP

---
+++ 1_sequencial_structure/class_6.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+</head>
+<body>
+
+<?php 
+
+$name = "Steve";
+$idade = "17";
+$cidade = "Osaka";
+
+echo $name . $idade . $cidade;
+
+$number = 2;
+
+if ($number % 2 == 0) {
+
+ echo "O número é par";
+
+} else {
+
+ echo "O número é ímpar";
+}
+
+
+function sum($sum1, $sum2) {
+
+echo $sum1 + $sum2;
+
+}
+
+sum();
+
+?>
+
+</body>
+</html>
