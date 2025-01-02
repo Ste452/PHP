68878172cdd7f7af6b00539a4c45b437be476971
Steve <falecomstevevaz@gmail.com>
2025-01-01 22:18:26 -0300

    Practicing functions, conditionals, arrays methods and foreach.

--- 1_sequencial_structure/class_6.php
+++ 1_sequencial_structure/class_6.php
@@ -8,26 +8,26 @@
 <?php 
 
 $name = "Steve";
-$idade = "17";
-$cidade = "Osaka";
+$age = "17";
+$city = "Osaka";
 
-echo $name . " " . $idade . " " . $cidade . "<br>";
+echo $name . " " . $age . " " . $city . "<br>";
 
 $number = 3;
 
 if ($number % 2 == 0) {
 
- echo "O número $number é par<br>";
+ echo "The number $number is even<br>";
 
 } else {
 
- echo "O número $number é ímpar<br>";
+ echo "The number $number is odd<br>";
 }
 
 
 function sum($sum1, $sum2) {
 
-echo "A soma de $sum1 e $sum2 é" . ($sum1 + $sum2) . "<br>";
+echo "The sum of $sum1 and $sum2 is " .  ($sum1 + $sum2)  . "<br>";
 
 }
 
@@ -35,26 +35,68 @@ sum(5, 10);
 
 function multiply($product1, $product2) {
 
-     echo "A multiplicação de $product1 e $product2 é" . $product1 * $product2 . "<br>"; 
+     echo "The multiplicity of $product1 and $product2 is" . $product1 * $product2 . "<br>"; 
 }
 
 multiply(2, 8);
 
-function subtract($minuendo, $subtraendo) {
+function subtract($minuend, $subtrahend) {
 
-   echo "A subtração de $minuendo e $subtraendo é" . ($minuendo - $subtraendo) . "<br>";
+   echo "A subtração de $minuend e $subtrahend é" . ($minuend - $subtrahend) . "<br>";
 }
 
 subtract(22, 5);
 
-function divide($dividendo, $divisor) {
+function divide($dividend, $divisor) {
 
-echo "A divisão de $dividendo e $divisor é" . ($dividendo / $divisor) . "<br>";
+     if ($divisor == 0) {
 
+        echo "Error: Impossible division by zero. <br>";      
+
+      } else {
+
+         echo "The division in $dividend e $divisor é" . ($dividend / $divisor) . "<br>";
+      }   
 }
 
 divide(34, 17);
 
+$array = [1, 2, 3, 4, 5];
+
+$isValidArray = true;
+
+foreach ($array as $valor) {
+
+  if (!is_numeric($value)) {
+     $isValidArray = false;
+     break;
+  }
+}
+
+if ($isValidArray){
+  
+  echo "All value of array are numbers.<br>";
+
+  for ($i = 1; $i <= count($array); $i++) {
+    
+    echo "The value in $i position is:" . $array[i] . "<br>";
+  
+}
+
+  $sum = array_sum($array);
+  echo "The sum of all numbers of array is: " . $array[i] . "<br>";
+} else {
+  echo "The array contain non numeric values.";
+}
+
+function arrayPosition() {
+
+    echo "The initial position of array: " . $array[0];
+    echo "The final position of array is: " . $array[cout($array) - 1] . "<br>";
+
+}
+
+arrayPosition();
 ?>
 
 </body>
