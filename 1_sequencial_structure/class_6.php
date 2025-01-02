5b9f14487e48b631dae863b87413c4d4ffe9e034
Steve <falecomstevevaz@gmail.com>
2025-01-01 21:58:27 -0300

    Adding function calculation

--- 1_sequencial_structure/class_6.php
+++ 1_sequencial_structure/class_6.php
@@ -11,27 +11,49 @@ $name = "Steve";
 $idade = "17";
 $cidade = "Osaka";
 
-echo $name . $idade . $cidade;
+echo $name . " " . $idade . " " . $cidade . "<br>";
 
-$number = 2;
+$number = 3;
 
 if ($number % 2 == 0) {
 
- echo "O número é par";
+ echo "O número $number é par<br>";
 
 } else {
 
- echo "O número é ímpar";
+ echo "O número $number é ímpar<br>";
 }
 
 
 function sum($sum1, $sum2) {
 
-echo $sum1 + $sum2;
+echo "A soma de $sum1 e $sum2 é" . ($sum1 + $sum2) . "<br>";
 
 }
 
-sum();
+sum(5, 10);
+
+function multiply($product1, $product2) {
+
+     echo "A multiplicação de $product1 e $product2 é" . $product1 * $product2 . "<br>"; 
+}
+
+multiply(2, 8);
+
+function subtract($minuendo, $subtraendo) {
+
+   echo "A subtração de $minuendo e $subtraendo é" . ($minuendo - $subtraendo) . "<br>";
+}
+
+subtract(22, 5);
+
+function divide($dividendo, $divisor) {
+
+echo "A divisão de $dividendo e $divisor é" . ($dividendo / $divisor) . "<br>";
+
+}
+
+divide(34, 17);
 
 ?>
 
