371b79f3bd2e3f4c77fecee1bebbcf32e1363bdc
Steve <falecomstevevaz@gmail.com>
2025-01-06 22:01:43 -0300

    Global variables

---
+++ 3_functions/class_18.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <?php 
+  
+    
+    $_SERVER 
+
+    $_GET
+
+    $_POST
+   
+    $_FILES
+
+    $_COOKIE
+
+     
+    $_SESSION 
+  ?>
+
+</body>
+</html>
